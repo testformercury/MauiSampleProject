540b1f6e3f22777bb57028848b20c3527e4249b9
sivanesh.j <sivanesh.j@intelizign.com>
2024-10-23 17:14:54 +0530

    Added Sample Project in the Repo

---
+++ JenkinsSampleMaui/JenkinsSampleMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JenkinsSampleMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ JenkinsSampleMaui/JenkinsSampleMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JenkinsSampleMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
